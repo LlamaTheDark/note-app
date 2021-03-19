cac946fed9add37bcb6b1b85a800bb4597dfcc37
LlamaTheDark <llamathedark@gmail.com>
2021-03-19 14:21:05 -0600

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="home">
+    
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    </div>
 </template>
 
 <script>
